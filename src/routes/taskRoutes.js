edf0dda521a61512891c75dae250e631ea062ea6
Ashutosh <aa8781@srmist.edu.in>
2025-03-25 10:14:04 +0530

    .

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {createTask,approveTask,updateTaskByAssigner}  from '../controller/taskController.js';
 const router =express.Router();
 
+
 router.post("/createTask/:assignBy",createTask);
 router.put("/accptedTask/:taskId",approveTask);
 router.put("/updateTask/:userId/:taskId",updateTaskByAssigner)
