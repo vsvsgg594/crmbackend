0b99d2d4d8703693d4701aa20275090f8aa243e7
vsvsgg594 <vsvsgg594@gmail.com>
2025-03-25 10:04:48 +0530

    added findTskbyid api and getALLtask api

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createTask,approveTask,updateTaskByAssigner,getAllTask}  from '../controller/taskController.js';
+import {createTask,approveTask,updateTaskByAssigner,getAllTask,findTaskByEmpId}  from '../controller/taskController.js';
 const router =express.Router();
 
 router.post("/createTask/:assignBy",createTask);
 router.put("/accptedTask/:taskId",approveTask);
 router.put("/updateTask/:userId/:taskId",updateTaskByAssigner);
 router.get("/gettask",getAllTask);
+router.get("/gettask/:empId",findTaskByEmpId);
 
 export default router;
